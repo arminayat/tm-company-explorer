d9e583641677d29448eb1f078f85779e9685ad57
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-24 08:42:09 +0200

    feat(API): add react-query query client

---
+++ src/services/queryClient.ts
@@ -0,0 +1,6 @@
+import { QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
+});
+export default queryClient;
