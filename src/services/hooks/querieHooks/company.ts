8c52114e079e88e54fcafb8ace08585a4c46d0a6
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-25 01:16:02 +0200

    feat(services/queries): add company-useGetById queryHook

--- src/services/hooks/querieHooks/company.ts
+++ src/services/hooks/querieHooks/company.ts
@@ -3,16 +3,27 @@ import { AxiosResponse } from 'axios';
 import { TCompany } from '../../../types';
 import API from '../../API';
 
-const fetchCompanies = async () => {
+const fetchAllCompanies = async () => {
   const { data }: AxiosResponse<TCompany[]> = await API.get('/companies');
+  return data;
+};
+
+const fetchCompanyById = async (id: string) => {
+  const { data }: AxiosResponse<TCompany> = await API.get(`/company/${id}`);
   return data;
 };
 
 export const company = {
   useGetAll: (options?: UseQueryOptions<TCompany[]>) => {
-    return useQuery<TCompany[]>(['company'], fetchCompanies, {
-      placeholderData: [],
+    return useQuery<TCompany[]>(['company'], fetchAllCompanies, {
       ...options,
     });
+  },
+  useGetById: (id: string, options?: UseQueryOptions<TCompany>) => {
+    return useQuery<TCompany>(
+      ['company', id],
+      () => fetchCompanyById(id),
+      options
+    );
   },
 };
