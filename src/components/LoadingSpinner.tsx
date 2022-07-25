84f7e716a8f942ad710641897b19af3a5001bdc7
Armin Ayatollahi <arminayat3@gmail.com>
2022-07-25 10:27:49 +0200

    feat: add LoadingSpinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+import { Row, Spin } from 'antd';
+import React from 'react';
+
+const LoadingSpinner = () => (
+  <Row align="middle" justify="center">
+    <Spin />
+  </Row>
+);
+
+export default LoadingSpinner;
